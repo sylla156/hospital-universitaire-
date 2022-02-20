e61643c4dfa553aa4c1471e0c3172b49d38d00b4
sylla ibrhim <s67986601@gmail.com>
2022-02-20 12:28:49 +0000

    build:about

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.static( __dirname + '/public/style/css'));
+app.use(express.static( __dirname + '/public/img'));
+
 
 app.set("views engine ", 'ejs');
 
