bfa0943f9c6de8ea9a1f06e947cabaa2bc04733a
sylla ibrhim <s67986601@gmail.com>
2022-02-20 19:49:32 +0000

    build:interfasePerfect

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 const element = document.querySelector(".app_about--img img");
 VanillaTilt.init(element);
+
+
+const login = document.querySelector('.app__login');
+const btnOut = document.querySelector('.exit')
+const btnInt = document.querySelector('.app__header--nav')
+
+btnOut.addEventListener('click', (e) => {
+    login.style.transform = 'translateX(100vw)';
+})
+
+
+btnInt.addEventListener('click', (e) => {
+    let  width = innerWidth;
+    login.style.transform = 'translate(0,0)';
+    
+})
