554901ee6cafafe80dec35d55854e4e3618b1ec8
sylla ibrhim <s67986601@gmail.com>
2022-02-20 15:44:15 +0000

    buildheader

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static( __dirname + '/public/style/css'));
-app.use(express.static( __dirname + '/public/img'));
+app.use(express.static(__dirname + "/public/style/css"));
+app.use(express.static(__dirname + "/public/img"));
+app.use(express.static(__dirname + "/public/js"));
 
-
-app.set("views engine ", 'ejs');
-
-
-
+app.set("views engine ", "ejs");
 
 app.get("/", (request, response) => {
-  response.render('index.ejs', {foo: 'FOO'});
+  response.render("index.ejs", { foo: "FOO" });
 });
 
-app.listen(3000)
+
+app.listen(3000);
