c0ccc1464210bc8e2ab9a058e8848576f93e25ff
sylla ibrhim <s67986601@gmail.com>
2022-02-21 09:43:51 +0000

    build:updateBtn

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 const element = document.querySelector(".app_about--img img");
 VanillaTilt.init(element);
 
+const pathname = location.pathname;
 
-const login = document.querySelector('.app__login');
-const btnOut = document.querySelector('.exit')
-const btnInt = document.querySelector('.app__header--nav')
+if (pathname === "/") {
+  const login = document.querySelector(".app__login");
+  const btnOut = document.querySelector(".exit");
+  const btnInt = document.querySelector(".app__header--nav");
 
-btnOut.addEventListener('click', (e) => {
-    login.style.transform = 'translateX(100vw)';
-})
+  btnOut.addEventListener("click", (e) => {
+    login.style.transform = "translateX(100vw)";
+  });
 
+  btnInt.addEventListener("click", (e) => {
+    let width = innerWidth;
+    login.style.transform = "translate(0,0)";
+  });
+} else if (pathname === "/admis") {
+  const login = document.querySelector(".admis__login");
+  const btnOut = document.querySelector(".exit");
+  const btnInt = document.querySelector(".update");
 
-btnInt.addEventListener('click', (e) => {
-    let  width = innerWidth;
-    login.style.transform = 'translate(0,0)';
-    
-})
+  btnOut.addEventListener("click", (e) => {
+    login.style.transform = "translateX(100vw)";
+  });
+
+  btnInt.addEventListener("click", (e) => {
+    e.preventDefault();
+    let width = innerWidth;
+    login.style.transform = "translate(0,0)";
+  });
+}
